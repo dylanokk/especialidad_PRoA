30cda14aea5b2a23afaf23958bf7dc55d6f33c06
Dylanokk <doviedo@escuelasproa.edu.ar>
2025-06-10 15:33:44 -0300

    Corrigiendo carpeta programacion, ya no es submódulo

---
+++ programacion/bibliotecas/ejercicio192.py
@@ -0,0 +1,16 @@
+class Cuadrado:Add commentMore actions
+    def __init__(self, lado):
+        self.lado = lado
+
+    def imprimir_perimetro(self):
+        perimetro = self.lado * 4
+        print(f"El perímetro del cuadrado es: {perimetro}")
+
+    def imprimir_superficie(self):
+        superficie = self.lado ** 2
+        print(f"La superficie del cuadrado es: {superficie}")
+
+# Ejemplo de uso
+cuadrado = Cuadrado(5)
+cuadrado.imprimir_perimetro()
+cuadrado.imprimir_superficie()
